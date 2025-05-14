5ca6d5b8871a4fcf37d717fb7ca78c95145a9c91
markvncent <markvncent@github.com>
2025-05-14 21:07:07 +0800

    Removed unused batch files

--- test_card_panel.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Create bin directory if it doesn't exist
-if not exist bin mkdir bin
-
-REM Compile only the required classes
-javac -d bin src/frontend/search/TestCardPanel.java src/frontend/search/CandidateCardPanel.java src/frontend/search/CandidateCard.java src/backend/model/CandidateDataLoader.java
-
-REM Run the test application
-java -cp bin frontend.search.TestCardPanel 
